0ad0a7c1d2c0801ccde558f5f03e245c518e0b64
H Fisher <hugo.fisher@gmail.com>
2026-07-04 16:42:44 +1000

    Minor code cleanup

--- Py/julia.py
+++ Py/julia.py
@@ -14,6 +14,7 @@ import PIL
 from PIL import Image
 
 def julia(x, y, size, scale, iterations):
+    """Julia calculation for single point, return color"""
     jx = (x - size/2.0) / (scale * 0.5 * size)
     jy = (y - size/2.0) / (scale * 0.5 * size)
     c = complex(-0.8, 0.156)
@@ -25,30 +26,31 @@ def julia(x, y, size, scale, iterations):
             return 0
     return 0x0000FF
 
-def fractal(img, size):
+def fractal(size):
+    """Create fractal image size x size"""
+    img = Image.new("RGB", (size, size), "white")
     pixels = img.load()
     for y in range(size):
         for x in range(size):
             pixels[(x, y)] = julia(x, y, size, 10, 200)
+    return img
 
 def main(argv):
     # Setup
-    size = 4096 # 1024
+    size = 1024 # 4096
     if len(argv) > 1:
         N = int(argv[1])
     else:
         N = 10
-    # Create image from (last) fractal
-    fName = "./fractal.png"
-    img = Image.new("RGB", (size, size), "white")
     # Just measure fractal creation
     timeBase = time.time()
     for i in range(N):
         print(i + 1)
-        fractal(img, size)
+        img = fractal(size)
     now = time.time()
     print("Average generate time {:4.3f} secs".format((now - timeBase)/N))
-    # Done
+    # Save last fractal for checking
+    fName = "./fractal.png"
     img.save(fName)
     print("Saved to {}".format(fName))
 
